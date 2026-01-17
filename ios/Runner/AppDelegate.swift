8026956fac35bfe05eb98cfe6d045aa3031e7e76
Amayuru1999 <amarasingheau@gmail.com>
2026-01-17 09:46:21 +0530

    feat: Integrate CocoaPods for macOS and add the geolocator_apple plugin.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,9 +9,8 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
 
-    // TODO: Replace with your Google Maps API key
-    // Get your key from: https://console.cloud.google.com/google/maps-apis
-    GMSServices.provideAPIKey("YOUR_GOOGLE_MAPS_API_KEY_HERE")
+   
+    GMSServices.provideAPIKey("GOOGLE_MAPS_API_KEY_HERE")
 
   
     GeneratedPluginRegistrant.register(with: self)
